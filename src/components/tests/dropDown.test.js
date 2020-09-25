f18a5127549a4e847e3760bec710bf1d7c974bf2
Neeraj Kumar Singh <neerajkumar719714@gmail.com>
2020-09-25 15:00:44 +0530

    new tests

--- src/components/tests/dropDown.test.js
+++ src/components/tests/dropDown.test.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { configure, shallow } from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import DropDown from '../DropDown';
-
-configure({adapter: new Adapter()});
 
 describe('<Dropdown/>',()=>{
     it('renders correctly (for two items in list)',()=>{
