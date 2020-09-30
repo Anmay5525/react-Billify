aa2f97a3a8eb648c23f50d56ddb44bd577f2f4fc
Anmay Kumar <anmayk1997@gmail.com>
2020-09-30 13:24:32 +0530

    cleanup

--- src/components/tests/rightPanel.test__.js
+++ src/components/tests/rightPanel.test__.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import RightPanel from '../RightPanel';
+import React from "react";
+import renderer from "react-test-renderer";
+import RightPanel from "../RightPanel";
 
-it('renders correctly',()=>{
-    const tree=renderer.create(<RightPanel/>).toJSON();
-    expect(tree).toMatchSnapshot();
+it("renders correctly", () => {
+  const tree = renderer.create(<RightPanel />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
