d346f7a68a7e4c61ac1b5828ce78999b6ee12228
ShivamRZP <shivam.murmu@razorpay.com>
2020-09-30 16:39:08 +0530

    Lint test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import '../App';
+import React from "react";
+import { mount } from "enzyme";
+import "../App";
+import App from "../App";
 
-describe('Testing App component if it', () => {
-  
-  it('renders without crashing', () => {
-    const container = mount(<InvoicesList />);
+describe("Testing App component if it", () => {
+  it("renders without crashing", () => {
+    const container = mount(<App />);
   });
-
 });
